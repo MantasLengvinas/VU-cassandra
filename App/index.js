a5cabe8a4256e3145c7058cce0c2f682b29ce7c2
Mantas Lengvinas <mantaslengvin@gmail.com>
2022-11-05 19:17:52 +0200

    queries result logic

--- App/index.js
+++ App/index.js
@@ -1,22 +1,12 @@
-const cassandra = require('cassandra-driver');
-
-const client = new cassandra.Client({
-    contactPoints: ['127.0.0.1'],
-    localDataCenter: 'datacenter1',
-    keyspace: 'Chat',
-});
+let groupsQueries = require("../Queries/group.js")
 
 let main = async () => {
+
     try {
     
-    client.connect();
-
-    // Cia tik testuo, tai krc reikia funkcijos, kuri callina querius, 
-    // ir tada queriai bus funkcijos, kurios tures param client tada grazint tsg rezultata ir printins cia
-    
-    const result = await client.execute("SELECT * FROM user");
-    result.rows.forEach((row) => console.log({ ...row }));
-
+        await groupsQueries.getGroupById();
+        await groupsQueries.getGroupByPrivacy();
+        await groupsQueries.getAllMembersInGroup();
     }
     catch(err) {
         console.log(err);
