5c886b7d83e277ae67062ed1ac31801d869990dd
gustaz <gustas2001@mail.ru>
2022-11-05 20:39:03 +0200

    Added diagrams

--- App/index.js
+++ App/index.js
@@ -1,4 +1,6 @@
-let groupsQueries = require("../Queries/group.js")
+let groupsQueries = require("../Queries/group.js");
+let userQueries = require("../Queries/user.js");
+let messageQueries = require("../Queries/message.js");
 
 let main = async () => {
 
@@ -7,6 +9,13 @@ let main = async () => {
         await groupsQueries.getGroupById();
         await groupsQueries.getGroupByPrivacy();
         await groupsQueries.getAllMembersInGroup();
+        await userQueries.getUserById();
+        await userQueries.getUserByEmail();
+        await userQueries.getUserByPhone();
+        await messageQueries.getMessageById();
+        await messageQueries.getMessageByAuthor();
+        await messageQueries.getMessageByChatroom();
+        await messageQueries.getMessageBetween();
     }
     catch(err) {
         console.log(err);
