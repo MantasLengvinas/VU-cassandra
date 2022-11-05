6f7d5fde7e24685a08a4e96efbb4553ac1a6feb7
Mantas Lengvinas <mantaslengvin@gmail.com>
2022-11-05 17:03:19 +0200

    connected to cassandra

--- App/index.js
+++ App/index.js
@@ -1,1 +1,26 @@
-console.log('hello world')
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+    contactPoints: ['127.0.0.1'],
+    localDataCenter: 'datacenter1',
+    keyspace: 'Chat',
+});
+
+let main = async () => {
+    try {
+    
+    client.connect();
+
+    // Cia tik testuo, tai krc reikia funkcijos, kuri callina querius, 
+    // ir tada queriai bus funkcijos, kurios tures param client tada grazint tsg rezultata ir printins cia
+    
+    const result = await client.execute("SELECT * FROM user");
+    result.rows.forEach((row) => console.log({ ...row }));
+
+    }
+    catch(err) {
+        console.log(err);
+    }
+}
+
+main();
